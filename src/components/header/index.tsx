45fc54f94d4053e9003813524fa8be7c8862a095
Hanna <hanna.velten@gmail.com>
2026-02-04 08:41:52 -0300

    refactor: altera divs para links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,26 +10,26 @@ const links = [
 
 export default function Header(){
     return (
-        <header className="bg-[#0F1C53] sticky top-0 z-20 w-full px-10 py-5 flex justify-between align-middle">
-            <div className="flex w-full">
+        <header className="bg-[#0F1C53] sticky top-0 z-20 w-full px-15 flex justify-center items-center">
+            <div className="flex flex-1">
                 <Link href='/'>
                     <Image 
                     src={'/imagens/logo.png'}
-                    alt="Logo do blog"
+                    alt="Logo da marca"
                     width={904}
                     height={904}
-                    className="h-25 w-40"
+                    className="h-25 w-auto"
                     />
                 </Link>
             </div>
-                <div className="flex gap-4 w-full justify-end h-full">
-                    <div>Home</div>
-                    <div>Produtos</div>
-                    <div>Contato</div>
-                    <div>Gerenciamento</div>
-                    <div>Carrinho</div>
+                <div className="flex flex-1 gap-14 w-full h-full text-[#FFFFFF] font-medium items-center">
+                    <Link href="/" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Home</Link>
+                    <Link href="/produtos" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Produtos</Link>
+                    <Link href="/contato" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Contato</Link>
+                    <Link href="/gerenciamento" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Gerenciamento</Link>
+                    <Link href="/carrinho" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Carrinho</Link>
+                    <div className=" bg-[#C9A24D] text-black font-medium rounded-full px-2 py-1 hover:bg-[#8184AC] hover:text-white rounded-full px-2 py-1 transition-colors duration-300">Login</div>
                 </div>
-
         </header>
     )
 }
