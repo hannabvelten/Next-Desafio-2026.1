2834edf3aff5e3cbb326a6abdb1ae9c188c64b85
Hanna <hanna.velten@gmail.com>
2026-02-06 17:53:33 -0300

    feat(navbar): implementa lógica de abrir e fechar navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from "react"
 
 import Link from "next/link"
 import Image from "next/image"
@@ -8,22 +10,38 @@ const links = [
 ]
 
 export default function Header(){
+
+    const [isNavOpen, setIsNavOpen] = useState(false)
+
     return (
-        <header className="bg-[#0F1C53] sticky top-0 z-20 w-full px-6 md:px-15 flex justify-center items-center">
-            <div className="icone text-white text-2xl md:hidden">
-                <i className="bi bi-list"></i>
+        <header className="bg-[#0F1C53] sticky top-0 z-20 w-ful px-6 md:px-15 flex flex-col ">
+            <div className="flex">
+                <button className="icone text-white text-2xl md:hidden text-start" onClick={() => setIsNavOpen(!isNavOpen)}>
+                    <i className="bi bi-list"></i>
+                </button>
+                <div className="flex flex-1 justify-center md:justify-start ">
+                    <Link href='/'>
+                        <Image 
+                        src={'/imagens/logo.png'}
+                        alt="Logo da marca"
+                        width={904}
+                        height={904}
+                        className="h-16 md:h-25 w-auto"
+                        />
+                    </Link>
+                </div>
             </div>
-            <div className="flex flex-1 justify-center md:justify-start">
-                <Link href='/'>
-                    <Image 
-                    src={'/imagens/logo.png'}
-                    alt="Logo da marca"
-                    width={904}
-                    height={904}
-                    className="h-16 md:h-25 w-auto"
-                    />
-                </Link>
-            </div>
+
+            {isNavOpen ? (
+                <div className="md:hidden flex flex-col gap-4 bg-[#0F1C53] text-white px-1 py-4">
+                    <Link href="/">Home</Link>
+                    <Link href="/produtos">Produtos</Link>
+                    <Link href="/contato">Contato</Link>
+                    <Link href="/gerenciamento">Gerenciamento</Link>
+                    <Link href="/carrinho">Carrinho</Link>
+                </div>
+            ) : null}
+
             <div className="hidden md:flex flex-1 gap-14 w-full h-full text-[#FFFFFF] font-medium items-center">
                 <Link href="/" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Home</Link>
                 <Link href="/produtos" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Produtos</Link>
@@ -32,6 +50,8 @@ export default function Header(){
                 <Link href="/carrinho" className="hover:bg-[#c8b473] hover:text-black rounded-full px-2 py-1 transition-colors duration-300">Carrinho</Link>
                 <div className=" bg-[#C9A24D] text-black font-medium rounded-full px-2 py-1 hover:bg-[#8184AC] hover:text-white rounded-full px-2 py-1 transition-colors duration-300">Login</div>
             </div>
+
+
 
         </header>
     )
