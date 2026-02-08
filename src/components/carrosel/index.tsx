412d9b68d56101c45b7fc0f25ff334735ccbd3d6
Hanna <hanna.velten@gmail.com>
2026-02-07 21:27:58 -0300

    style: faz desing do primeiro card do carrosel

--- src/components/carrosel/index.tsx
+++ src/components/carrosel/index.tsx
@@ -21,27 +21,38 @@ export default function Carrossel() {
 
         <div className="container flex">
 
-          <div className="card flex-[0_0_100%] md:flex-[0_0_33%] p-3">
-            <Card>
-              <CardContent className="p-0">
+          <div className="card flex-[0_0_100%] md:flex-[0_0_33%] p-3 flex justify-center items-center">
+            <Card className='w-50 h-70 p-0'>
+              <CardContent className="p-0 w-full ">
                 <Link href='/'>
                         <Image 
                         src={'/imagens/anelDuoCravejado.jpg'}
                         alt="foto anel cravejado"
                         width={904}
                         height={904}
-                        className="h-auto w-full"
+                        className="h-auto w-full rounded-t-xl"
                         />
                     </Link>
-                <h1 className='font-medium'>Anel Duo Cravejado</h1>
+                <h1 className='font-medium px-6 pb-2 pt-4'>Anel Duo Cravejado</h1>
+                <p className='px-6'>R$350,00</p>
               </CardContent>
             </Card>
           </div>
 
           <div className="card flex-[0_0_100%] md:flex-[0_0_33%] p-3">
-            <Card>
-              <CardContent className="p-4">
-                Produto 2
+            <Card className='w-50 h-70 p-0'>
+              <CardContent className="p-0 w-full ">
+                <Link href='/'>
+                        <Image 
+                        src={'/imagens/anelDuoCravejado.jpg'}
+                        alt="foto anel cravejado"
+                        width={904}
+                        height={904}
+                        className="h-auto w-full rounded-t-xl"
+                        />
+                    </Link>
+                <h1 className='font-medium px-6 pb-2 pt-4'>Anel Duo Cravejado</h1>
+                <p className='px-6'>R$350,00</p>
               </CardContent>
             </Card>
           </div>
