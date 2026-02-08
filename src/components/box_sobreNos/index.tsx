25f5815d5d2bcfb132ba9efff9e0679eefa37d3e
Hanna <hanna.velten@gmail.com>
2026-02-08 11:04:19 -0300

    feat: finaliza responsividade do componente box_sobreNos para telas medias

--- src/components/box_sobreNos/index.tsx
+++ src/components/box_sobreNos/index.tsx
@@ -4,13 +4,13 @@ import Image from "next/image"
 
 export default function Box_SobreNos() {
     return (
-        <div className="tudo w-full bg-brancoGelo flex px-4 md:px-20">
-            <div className="bg-amarelinho w-full md:h-150 rounded-3xl flex">
-                <div className="texto p-6">
-                    <h1 className="font-anticdidone pb-2 text-azulEscuro font-bold text-xl md:text-6xl md:p-20">Elegância em <br />cada detalhe</h1>
-                    <p className="text-xs">Do desenho ao acabamento final, nossas joias refletem sofisticação e cuidado. <span className="pt-2">Peças versáteis, feitas para durar e brilhar em qualquer ocasião.</span></p>
+        <div className="tudo w-full bg-brancoGelo flex px-4 md:px-20 md:py-8">
+            <div className="bg-amarelinho w-full md:h-150 rounded-3xl flex h-70">
+                <div className="texto p-6 md:w-130 w-130">
+                    <h1 className="font-anticdidone pb-2 text-azulEscuro font-bold text-xl md:text-6xl md:p-10 md:whitespace-nowrap">Elegância em <br />cada detalhe</h1>
+                    <p className="text-xs md:text-3xl md:px-11 md:tracking-wider">Do desenho ao acabamento final, nossas joias refletem sofisticação e cuidado. <br /> Peças versáteis, feitas para durar e brilhar em qualquer ocasião.</p>
                 </div>
-                <Link href='/' className="relative">
+                <Link href='/' className="relative w-150 md:w-full">
                     <Image 
                     src={'/imagens/mulherHome.jpg'}
                     alt="mulher com colar"
