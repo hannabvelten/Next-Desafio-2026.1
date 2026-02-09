a3b9d362aa6008454d57688d96b8606bff251c7c
Hanna <hanna.velten@gmail.com>
2026-02-09 14:55:00 -0300

    style: ajusta card espelhado em telas pequenas

--- src/components/card-espelhado/index.tsx
+++ src/components/card-espelhado/index.tsx
@@ -6,17 +6,7 @@ import { Button } from "@/components/ui/button"
 export default function CardEspelhado () {
     return (
         <div>
-            <div className="card w-full h-60 flex px-6 gap-5 pt-8 items-center md:justify-end">
-                <div className="conteudo flex flex-col gap-2 md:justify-end">
-                    <h1 className="text-[18px] md:text-2xl font-medium flex md:justify-end">Brinco Argola + Estrela</h1>
-                    <p className="text-[18px] md:text-2xl flex md:justify-end">R$1.290,00</p>
-                    <p className="text-[11px] md:text-xs flex md:justify-end">ou 5x de R$258,00</p>
-                    <div className="w-full flex justify-end">
-                        <Button className="bg-creme flex justify-center rounded-3xl p-1 w-24 hover:bg-amber-100 text-black font-light" variant="default">
-                            <p className="text-[14px] font-regular">VER MAIS</p>
-                        </Button>
-                    </div>
-                </div>
+            <div className="card w-full h-60 flex px-6 gap-5 pt-8 items-center md:justify-start md:flex-row-reverse">
                 <Link href='/' className="" >
                     <Image 
                     src={'/imagens/brincoEstrela.jpeg'}
@@ -26,6 +16,16 @@ export default function CardEspelhado () {
                     className="object-cover h-40 w-40 md:h-50 md:w-50"
                     />
                 </Link>
+                <div className="conteudo flex flex-col gap-2 md:justify-end">
+                    <h1 className="text-[18px] md:text-2xl font-medium flex md:justify-end">Brinco Argola + Estrela</h1>
+                    <p className="text-[18px] md:text-2xl flex md:justify-end">R$1.290,00</p>
+                    <p className="text-[11px] md:text-xs flex md:justify-end">ou 5x de R$258,00</p>
+                    <div className="w-full flex md:justify-end">
+                        <Button className="bg-creme flex justify-center rounded-3xl p-1 w-24 hover:bg-amber-100 text-black font-light" variant="default">
+                            <p className="text-[14px] font-regular">VER MAIS</p>
+                        </Button>
+                    </div>
+                </div>
     
             </div>
 
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -3,7 +3,7 @@ import { Search as SearchIcon} from "lucide-react";
 export default function Search() {
     return (
         <form className="flex basis-full px-3" autoComplete="off">
-            <div className=" flex w-full flex-col items-center gap-6 md:pt-5 md:pb-10">
+            <div className=" flex w-full flex-col items-center gap-6 pt-4 md:pt-5 md:pb-10">
                 <div className="w-full md:h-8 flex md:w-150 rounded-xl bg-creme shadow-md/20 px-5 py-2 text-xs gap-3 h-8">
                     <SearchIcon className="w-5 md:w-4 h-full opacity-60 flex items-center"/>
                     <input 
