96f9937f3bbf04eb90222981450b4156eaa57cff
Hanna <hanna.velten@gmail.com>
2026-02-11 17:18:53 -0300

    style: finaliza front-end da pag de produto individual

--- src/components/produto-individual/index.tsx
+++ src/components/produto-individual/index.tsx
@@ -6,19 +6,26 @@ import { ChevronDown } from 'lucide-react';
 
 export default function PagPIndivudual (){
     return (
-        <div className="tudo flex flex-col md:flex-row w-full justify-center p-10">
-            <div className="imagem py-10 md:w-100 w-full flex justify-center items-center">
-                <Link href='/'className="">
-                    <Image 
-                    src={'/imagens/aliancaOuro.jpeg'}
-                    alt="Aliança ouro"
-                    width={904}
-                    height={904}
-                    className="object-cover h-45 w-50 rounded-t-2xl md:w-100 md:h-100"
-                    />
-                </Link>
+        <div className="tudo flex flex-col md:flex-row w-full justify-center md:p-10">
+            <div className="flex flex-col">
+                <div className="imagem py-10 md:pt-10 md:py-0 md:w-100 w-full flex justify-center items-center">
+                    <Link href='/'className="">
+                        <Image 
+                        src={'/imagens/aliancaOuro.jpeg'}
+                        alt="Aliança ouro"
+                        width={904}
+                        height={904}
+                        className="object-cover h-45 w-55 rounded-t-2xl md:w-100 md:h-100"
+                        />
+                    </Link>
+                </div>
+
+                <div className="carrinho w-full md:flex justify-center pt-7 sm:hidden">
+                    <Button className="bg-golden text-black w-100 rounded-4xl">ADICIONAR AO CARRINHO</Button>
+                </div>
             </div>
-            <div className="conteudo flex flex-col gap-2 md:gap-6 px-10 md:w-150 md:justify-center">
+
+            <div className="conteudo flex flex-col gap-4 md:gap-6 px-10 md:w-150 md:justify-center">
                 <h1 className="text-2xl md:text-4xl font-semibold">Par Alianças Premium</h1>
 
                 <div className="w-full flex justify-center md:justify-start md:py-2">
@@ -28,7 +35,7 @@ export default function PagPIndivudual (){
                 <p className="text-xs md:text-lg">Par de alianças premium em ouro 18k, com acabamento refinado e design atemporal.
                 Uma joia criada para simbolizar união, amor e compromisso em todos os momentos da vida.</p>
 
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 ">
                     <p className="text-2xl md:text-3xl">R$4.590,00</p>
                     <p className="text-xs">ou 12x de R$382,50</p>
                 </div>
@@ -44,6 +51,10 @@ export default function PagPIndivudual (){
                 <div className="w-full flex justify-center md:justify-start md:py-0">
                     <div className="linha w-80 md:w-150 h-px bg-black"></div>
                 </div>
+            </div>
+
+            <div className="carrinho w-full flex justify-center pt-7 md:hidden">
+                <Button className="bg-golden text-black w-80 rounded-4xl">ADICIONAR AO CARRINHO</Button>
             </div>
         </div>
     )
