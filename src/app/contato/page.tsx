598824ea74c086adfc70607ad03ed69ac2cbb65b
Hanna <hanna.velten@gmail.com>
2026-02-11 17:54:08 -0300

    feat: cria componente contatos

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,9 @@
+import Contatos from "@/src/components/contatos";
+
+export default function Contato() {
+    return(
+        <div className="min-h-screen">
+            <Contatos />
+        </div>
+    )
+}
---
+++ src/components/contatos/index.tsx
@@ -0,0 +1,10 @@
+export default function Contatos() {
+    return (
+        <div className="w-full flex flex-col justify-center items-center p-7 gap-4">
+            <h1 className="text-3xl text-azulEscuro">Entre em Contato</h1>
+            <p className="text-center text-xs">Tem alguma dúvida, sugestão ou precisa de ajuda com seu pedido? Estamos aqui para te atender com carinho e atenção!</p>
+        </div>
+
+        
+    )
+}
