32e49f2ef47ef121925027270a95031dcd528d97
Hanna <hanna.velten@gmail.com>
2026-02-20 14:41:23 -0300

    style: estiliza o componente imagem-login

--- src/components/imagem-login/index.tsx
+++ src/components/imagem-login/index.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link"
+import Image from "next/image"
+
 export default function ImagemLogin (){
     return (
         <div>
-            
+            <Link href='/'>
+                <Image 
+                src={'/imagens/colarManequim.jpeg'}
+                alt="Colar em manequim"
+                width={904}
+                height={904}
+                className=" md:h-185 w-auto"
+                />
+            </Link>
+            <div className="absolute inset-0 bg-linear-to-r right-260 from-transparent via-transparent to-white"></div>
         </div>
     )
 }
---
+++ src/components/inputs-login/index.tsx
@@ -0,0 +1,7 @@
+export default function InputsLogin (){
+    return(
+        <div>
+            
+        </div>
+    )
+}
