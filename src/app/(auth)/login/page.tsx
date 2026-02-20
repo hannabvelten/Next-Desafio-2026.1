1af446a598bb4ed5441ffba6b1ac2bbbc931956a
Hanna <hanna.velten@gmail.com>
2026-02-20 14:40:44 -0300

    feat: cria a pasta (auth) e coloca o login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,11 @@
 import ImagemLogin from "@/src/components/imagem-login";
+import InputsLogin from "@/src/components/inputs-login";
 
 export default function Login (){
     return (
         <div>
             <ImagemLogin />
+            <InputsLogin />
         </div>
     )
 }
