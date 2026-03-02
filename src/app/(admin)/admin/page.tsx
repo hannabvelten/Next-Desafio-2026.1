2195b3a323c723e7fff442af7281eb84ebbb4c1e
Hanna <hanna.velten@gmail.com>
2026-03-01 22:49:46 -0300

    fix: next 15 searchParams

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -9,27 +9,34 @@ import { Plus } from "lucide-react"
 
 
 export default async function Page({
-        searchParams,
+    searchParams,
     }: {
-        searchParams:{
-            query?: string;
-            page?: string;
-        }
+    searchParams: Promise<{
+        query?: string
+        page?: string
+    }>
     }) {
-        const query = searchParams?.query || ''
-        const currentPage = Number(searchParams?.page) || 1
-        
-        const {products, count} = await fetchFilteredPosts(query, currentPage,5)
-        const itemsPerPage = 4
-        const totalPages = Math.ceil(count / itemsPerPage)
 
-    return(
+    const params = await searchParams
+
+    const query = params?.query || ''
+    const currentPage = Number(params?.page) || 1
+
+    const { products, count } = await fetchFilteredPosts(query, currentPage, 5)
+
+    const itemsPerPage = 4
+    const totalPages = Math.ceil(count / itemsPerPage)
+
+    return (
         <div className="w-full">
-            <div className="w-full py-10 px-10 md:px-14 flex flex-col md:flex-row md:justify-between gap-6">
-                <h1 className="text-xl md:text-3xl font-semibold">Gerenciamento de produtos</h1>
-                <Criar />  
-            </div>
-            <Tabela products = {products} totalPages={totalPages} />
+        <div className="w-full py-10 px-10 md:px-14 flex flex-col md:flex-row md:justify-between gap-6">
+            <h1 className="text-xl md:text-3xl font-semibold">
+            Gerenciamento de produtos
+            </h1>
+            <Criar />
+        </div>
+
+        <Tabela products={products} totalPages={totalPages} />
         </div>
     )
 }
